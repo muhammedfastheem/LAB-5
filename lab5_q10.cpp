5b75fc925d539bfed9d6a0326dd2939de5807aa8
muhammedfastheem <42205163+muhammedfastheem@users.noreply.github.com>
2018-09-02 09:03:07 +0000

    Update lab5_q10.cpp

--- lab5_q10.cpp
+++ lab5_q10.cpp
@@ -1,10 +1,13 @@
+//loading the library
 #include<iostream>
 using namespace std;
-
+//using the main function
 int main(){
+    //defining the variable
     char xy;
     cout<<"type the letter"<<endl;
     cin>> xy;
+    //condition
     if (xy>='A'&&xy<='Z')
     {
         cout<<"the letter is uppercase";
@@ -13,5 +16,6 @@ int main(){
     {
         cout<<"the letter is lowercase";
     }
+    //closing the program
     return 0;
 }
