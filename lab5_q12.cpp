ecfe0949563ad9c6ae6004c2107e1d308bfa3ff1
muhammedfastheem <42205163+muhammedfastheem@users.noreply.github.com>
2018-09-02 09:05:24 +0000

    Update lab5_q12.cpp

--- lab5_q12.cpp
+++ lab5_q12.cpp
@@ -1,10 +1,13 @@
+//including the library
 #include<iostream>
 using namespace std;
-
+//using the main function
 int main(){
+    //defining the variable
     int a;
     cout<<"type month number"<<endl;
     cin>> a;
+    //condition
     if (a==1||a==3||a==5||a==7||a==8||a==10||a==12)
     {
         cout<<"the month has 31 days";
@@ -17,5 +20,6 @@ int main(){
     {
         cout<<"the month has 30 days";
     }
+    //closing the program
     return 0;
 }
