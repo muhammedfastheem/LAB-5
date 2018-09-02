e6b42ca0686ca36c37ac375e9635a9abd65ad9ac
muhammedfastheem <42205163+muhammedfastheem@users.noreply.github.com>
2018-09-02 09:00:49 +0000

    Update lab5_q1.cpp

--- lab5_q1.cpp
+++ lab5_q1.cpp
@@ -1,12 +1,15 @@
+//including the library
 #include<iostream>
 using namespace std;
-
+//using the main function
 int main() {
+        //defining variables
         float a,b;
         cout<<"first number"<<endl;
         cin>>a;
         cout<<"second number"<<endl;
         cin>>b;
+        //condition
         if(a>b)
         {
             cout<<a<<"is greater";
@@ -15,6 +18,6 @@ int main() {
         {
             cout<<b<<"is greater";
         }
-
+//closing the program
 return 0;
 }
